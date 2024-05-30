36f318d4fb05a6e86ebb9323e53f5b2baa9eebf0
cam-vasquez <00065221@uca.edu.sv>
2024-05-30 11:52:34 -0600

    Tittle: Add No Wifi Connection View \n\n [BAdd Error View for error hadnling and initialize entries-history-view and related components for house owner history

---
+++ Client/src/pages/PageNotWifi.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "../components/buttons/Button";
+import SectionIntro from "../components/SectionIntro";
+/* import { Link } from "react-router-dom";*/
+import NotWifiImage from "../assets/images/not_wifi.svg";
+
+const PageNotWifi = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-2 p-8 text-center">
+      <img src={NotWifiImage} alt="not wifi connection" width={250} className=".h-auto  " />
+      <SectionIntro
+        title="¡Oh, no!"
+        subtitle="Al parecer no tienes conexión a Internet"
+        generalClassName="max-w-[30ch]"
+      />
+
+{/*       <Button as={Link} to="/home" className="mt-5">
+ */}
+      <Button  className="mt-5">
+        Regresar a Inicio
+      </Button>
+    </main>
+  );
+};
+
+export default PageNotWifi;
---
+++ Client/src/pages/history/EntriesHistory.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+/*Icons*/
+
+/*Components*/
+
+const EntriesHistory = () => {
+  return (
+    <div className="flex min-h-svh flex-1 flex-col items-center justify-center bg-white py-12 sm:px-6 lg:px-8">
+     
+      <p>Hello World</p>
+      
+
+
+    </div>
+  );
+};
+
+export default EntriesHistory;
