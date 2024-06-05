6ab2f449d47358a7fff08ffb930c4617adb9ba8c
Melvin Aguilar <90595158+MelvinAguilar@users.noreply.github.com>
2024-06-05 06:20:43 +0000

    Add temporaly links to home

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { TableComponent } from "../../components/table/GeneralTable";
-/*Icons*/
+import { Link } from "react-router-dom";
+import Button from "../../components/buttons/Button";
+import { VIEWS } from "../../lib/views";
+
+const Grid = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      {Object.keys(VIEWS).map((viewKey) => (
+        <Button 
+          key={viewKey} 
+          as={Link} 
+          to={VIEWS[viewKey]} 
+          className="mt-5 flex justify-center items-center w-full p-2 bg-blue-500 text-white rounded-lg"
+        >
+          {viewKey.replace(/([A-Z])/g, " $1").trim()}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
 
 const Home = () => {
   return (
-    <div className="flex min-h-svh flex-1 flex-col items-center justify-center bg-white py-12 sm:px-6 lg:px-8">
-      <TableComponent />
-    </div>
+    <>
+      <Grid />
+    </>
   );
 };
 
