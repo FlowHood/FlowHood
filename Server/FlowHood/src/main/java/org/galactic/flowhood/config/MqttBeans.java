e61c248fe61e09b5f757d694f19450fd0b143c24
HenryLima07 <00033721@uca.edu.sv>
2024-07-06 21:24:05 -0600

    start mqtt configuration

---
+++ Server/FlowHood/src/main/java/org/galactic/flowhood/config/MqttBeans.java
@@ -0,0 +1,18 @@
+package org.galactic.flowhood.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MqttBeans {
+//    @Bean
+//    public MqttPahoClientFactory mqttClientFactory() {
+//        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+//        MqttConnectOptions options = new MqttConnectOptions();
+//        options.setServerURIs(new String[] { "tcp://host1:1883", "tcp://host2:1883" });
+//        options.setUserName("username");
+//        options.setPassword("password".toCharArray());
+//        factory.setConnectionOptions(options);
+//        return factory;
+//    }
+}
