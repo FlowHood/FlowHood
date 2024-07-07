02ea91d2acfcfa148d933ea0cc3ce390673073d7
MelvinAguilar <00067621@uca.edu.sv>
2024-07-07 01:35:26 -0600

    update request list with admin user

--- Client/src/pages/admin/RequestList.jsx
+++ Client/src/pages/admin/RequestList.jsx
@@ -3,6 +3,7 @@ import { TableComponent } from "../../components/table/GeneralTable";
 import DashboardLayout from "../../components/dashboard/DashboardLayout";
 import SectionIntro from "../../components/SectionIntro";
 import { getAllRequests } from "../../services/request.service";
+import { Modal, Button, Avatar, List, Tag } from "antd";
 
 const requestTags = ["razon", "visitante", "residente", "fecha_inicio", "fecha_fin", "estado_solicitud"];
 const requestSearch = ["razon", "visitante", "residente"];
@@ -11,17 +12,38 @@ const requestFiltersOn = ["estado_solicitud"];
 
 const RequestList = () => {
   const [requestData, setRequestData] = useState([]);
+  const [requestDataRaw, setRequestDataRaw] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedRequest, setSelectedRequest] = useState(null);
 
   useEffect(() => {
     const fetchRequests = async () => {
       const data = await getAllRequests();
-      setRequestData(data);
+      setRequestDataRaw(data);
+      const filteredData = data.map((request) => {
+
+        // a data, quitarle los atributos resident,visitor,house y reason
+        const { resident, visitor, house, reason, ...rest } = request;
+        return rest;
+      });
+      setRequestData(filteredData);
       setLoading(false);
     };
 
     fetchRequests();
   }, []);
+
+  const handleView = (id) => {
+    const request = requestDataRaw.find((req) => req.id === id);
+    setSelectedRequest(request);
+    setIsModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalVisible(false);
+    setSelectedRequest(null);
+  };
 
   return (
     <DashboardLayout>
@@ -30,14 +52,60 @@ const RequestList = () => {
         {loading ? (
           <span>Cargando...</span>
         ) : (
-          <RequestTable data={requestData} />
+          <RequestTable data={requestData} onView={handleView} />
         )}
       </div>
+      <Modal
+        title="Información de la Solicitud"
+        visible={isModalVisible}
+        onCancel={handleModalClose}
+        footer={[
+          <Button key="close" onClick={handleModalClose}>
+            Cerrar
+          </Button>,
+        ]}
+      >
+        {selectedRequest && (
+          <div>
+            <div style={{ textAlign: "center", marginBottom: "20px", marginTop: "41px" }}>
+            <p>Razón: {selectedRequest.reason}</p>
+              <h2>{selectedRequest.resident.name}</h2> 
+              <p>Email visitante: {selectedRequest.visitor.email}</p>
+              <p>Fecha de inicio: {selectedRequest.fecha_inicio}</p>
+              {/* <p>Fecha de fin: {selectedRequest.fecha_fin}</p> */}
+              <p>Estado de la solicitud: {selectedRequest.estado_solicitud}</p>
+              
+            </div>
+            <div>
+              <h3>Casa</h3>
+              <p>Dirección: {selectedRequest.house.address}</p>
+              <p>Responsable: {selectedRequest.house.responsible.name}</p>
+              <p>Email del responsable: {selectedRequest.house.responsible.email}</p>
+              <div>
+                <h4>Residentes</h4>
+                <List
+                  itemLayout="horizontal"
+                  dataSource={selectedRequest.house.residents}
+                  renderItem={(resident) => (
+                    <List.Item>
+                      <List.Item.Meta
+                        avatar={<Avatar src={resident.picture} />}
+                        title={resident.name}
+                        description={"Email: " + resident.email}
+                      />
+                    </List.Item>
+                  )}
+                />
+              </div>
+            </div>
+          </div>
+        )}
+      </Modal>
     </DashboardLayout>
   );
 };
 
-const RequestTable = ({ data }) => {
+const RequestTable = ({ data, onView }) => {
   return (
     <TableComponent
       data={data}
@@ -45,6 +113,7 @@ const RequestTable = ({ data }) => {
       addSearchOn={requestSearch}
       addSortOn={requestSorter}
       addFiltersOn={requestFiltersOn}
+      onView={onView}
     />
   );
 };
